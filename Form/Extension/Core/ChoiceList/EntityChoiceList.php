1c318ce572c768c5b9424d53cf821b6cc2b582e8
clivez <clivez@nationalfibre.net>
2014-03-21 21:34:42 +0000

    DE101-1669 Introduced EntityChoiceList and moved CountryChoiceFormType to Base/GeographicalBundle

---
+++ Form/Extension/Core/ChoiceList/EntityChoiceList.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @copyright 2014 Instaclick Inc.
+ */
+
+namespace IC\Bundle\Base\ComponentBundle\Form\Extension\Core\ChoiceList;
+
+use Symfony\Component\Form\Extension\Core\ChoiceList\ObjectChoiceList;
+use IC\Bundle\Base\ComponentBundle\Entity\Entity;
+
+/**
+ * A ChoiceList that ONLY works with IC\Bundle\Base\ComponentBundle\Entity\Entity
+ *
+ * @author Clive Zagno <clivez@nationalfibre.net>
+ */
+class EntityChoiceList extends ObjectChoiceList
+{
+    /*
+     * {@inheritdoc}
+     */
+    protected function isPreferred($choice, array $preferredChoices)
+    {
+        if ( ! $choice instanceof Entity) {
+            throw new \InvalidArgumentException('Choice is not of type IC\Bundle\Base\ComponentBundle\Entity\Entity');
+        }
+
+        //var_dump($choice); exit;
+       // echo $choice->getId();
+        //var_dump(array_search($choice->getId(), $preferredChoices, true));
+
+        return false !== array_search($choice->getId(), $preferredChoices, true);
+    }
+}
---
+++ Tests/Form/Extension/Core/ChoiceList/EntityChoiceListTest.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * @copyright 2013 Instaclick Inc.
+ */
+
+namespace IC\Bundle\Base\ComponentBundle\Tests\Form\Extension\Core\ChoiceList;
+
+use IC\Bundle\Base\TestBundle\Test\TestCase;
+use IC\Bundle\Base\ComponentBundle\Form\Extension\Core\ChoiceList\EntityChoiceList;
+
+/**
+ * Test EntityChoiceList
+ *
+ * @group Unit
+ * @group Form
+ * @group FormExtension
+ *
+ * @author Clive Zagno <clivez@nationalfibre.net>
+ */
+class EntityChoiceListTest extends TestCase
+{
+    /**
+     * Test that a list of BaseComponent/Entity is able to be filteres by id
+     */
+    public function testPreffedViewsWithEntity()
+    {
+        $entityHelper = $this->getHelper('Unit\Entity');
+
+        $entities[] = $this->createEntity('US', 'United States');
+        $entities[] = $this->createEntity('CA', 'Canada');
+        $entities[] = $this->createEntity('ZA', 'South Africa');
+
+        $choiceList = new EntityChoiceList($entities, 'name', array('CA'), null, 'id');
+
+        $preferredViews = $choiceList->getPreferredViews();
+        $preferredView  = array_shift($preferredViews);
+
+        $this->assertEquals($preferredView->label, "Canada");
+        $this->assertEquals($preferredView->value, "CA");
+    }
+
+    /**
+     * Test that we can only allow BaseComponent/Entity
+     *
+     * @expectedException \InvalidArgumentException
+     */
+    public function testWithNonEntity()
+    {
+        $entity       =  new \stdClass();
+        $entity->name = 'name';
+
+        $choiceList = new EntityChoiceList(array($entity), 'name');
+    }
+
+    private function createEntity($id, $name)
+    {
+        $entityHelper = $this->getHelper('Unit\Entity');
+        $entity       = $entityHelper->createMock('IC\Bundle\Base\ComponentBundle\Entity\Entity', $id);
+        $entity->name = $name;
+
+        return $entity;
+    }
+}
